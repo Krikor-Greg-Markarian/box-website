6b7f48d6da9b1b018c0fc2f5349f162b2c76ba09
rony <rriachi@bright-lab.com>
2021-09-06 12:26:23 +0300

    large picture with some text

---
+++ src/components/Largebox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Largebox() {
+    return (
+        <div>
+            <div className = "">
+                <div className = "skateboard"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Largebox
