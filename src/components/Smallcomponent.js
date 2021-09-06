6d4905c7344499447a48d108f7cbde55b4ad7b08
rony <rriachi@bright-lab.com>
2021-09-06 17:20:08 +0300

    small components

---
+++ src/components/Smallcomponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Smallcomponent() {
+    return (
+        <div>
+          <div className = "smalldesign2"></div>
+          <p>Your Brand Plan Is More Important Than Your Business</p>
+          <p className = "text-gray-500 inline">BY </p>
+          <p className = "text-blue-500 font-bold inline">DEOTHEMES </p>
+          <p className = "text-gray-500 inline"> JAN 21, 2018</p>
+        </div>
+    )
+}
+
+export default Smallcomponent
