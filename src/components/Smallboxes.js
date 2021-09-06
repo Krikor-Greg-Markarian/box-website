89c75ced4df10f2ef9ef158515c0721eb94ba005
rony <rriachi@bright-lab.com>
2021-09-06 16:37:36 +0300

    small boxes

---
+++ src/components/Smallboxes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Smallboxes() {
+    return (
+        <div>
+            <div className = "applewatch"></div>
+        </div>
+    )
+}
+
+export default Smallboxes
