8fc7aee36a1ce3f8c4f08f4e922dbe9409082863
rony <rriachi@bright-lab.com>
2021-09-06 16:37:47 +0300

    boxes together

---
+++ src/components/Twolargefoursmall.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Twolargefoursmall() {
+    return (
+       
+           <div className = "">
+               <div className = "skateboard"></div>
+               
+           </div>
+        
+    )
+}
+
+export default Twolargefoursmall
