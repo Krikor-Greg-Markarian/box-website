c53c0f0ef6c61ce0d463a4c798750b2f92ee34c8
rony <rriachi@bright-lab.com>
2021-09-07 12:54:14 +0300

    newest videos

---
+++ src/components/Newset.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Newset() {
+    return (
+        <div className = "bg-white p-4 ">
+            <div className = "flex border-b-2">
+           <div className = "smalldesign "></div> 
+           <p className = "mb-2">Here are 10 books that will help you  grow a lasting business</p>
+           
+           </div>
+        </div>
+    )
+}
+
+export default Newset
