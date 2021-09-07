dfa5beb90230d280b32634c748c17bd7128c84f7
rony <rriachi@bright-lab.com>
2021-09-07 12:54:45 +0300

    small components

--- src/components/Smallcomponent.js
+++ src/components/Smallcomponent.js
@@ -4,10 +4,10 @@ function Smallcomponent() {
     return (
         <div>
           <div className = "smalldesign2"></div>
-          <p>Your Brand Plan Is More Important Than Your Business</p>
-          <p className = "text-gray-500 inline">BY </p>
-          <p className = "text-blue-500 font-bold inline">DEOTHEMES </p>
-          <p className = "text-gray-500 inline"> JAN 21, 2018</p>
+          <p className = "text-sm mt-2 mb-2">Your Brand Plan Is More Important Than Your Business</p>
+          <p className = "text-gray-500 inline text-sm ">BY </p>
+          <p className = "text-blue-500 font-bold inline text-sm">DEOTHEMES </p>
+          <p className = "text-gray-500 inline text-sm"> JAN 21, 2018</p>
         </div>
     )
 }
