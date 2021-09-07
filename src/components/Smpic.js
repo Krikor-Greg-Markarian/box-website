db057793b043ded671af24a26dbeaafba92a2ec1
rony <rriachi@bright-lab.com>
2021-09-07 12:54:34 +0300

    small pictures with text

--- src/components/Smpic.js
+++ src/components/Smpic.js
@@ -5,6 +5,7 @@ function Smpic() {
         <div className = "flex justify-center items-center mt-2">
             <div className="smallpicture"></div>
             <p className = "ml-2">it's Never Too Late to pursue your passion</p>
+            
         </div>
     )
 }
