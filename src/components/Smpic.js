5dec346dc9584cdad5323fd14f2921809b9f3cab
rony <rriachi@bright-lab.com>
2021-09-07 11:01:12 +0300

    small picture with text

---
+++ src/components/Smpic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Smpic() {
+    return (
+        <div className = "flex justify-center items-center mt-2">
+            <div className="smallpicture"></div>
+            <p className = "ml-2">it's Never Too Late to pursue your passion</p>
+        </div>
+    )
+}
+
+export default Smpic
