e0fab7c5fdc9975a1d20cd6c9a2d60982a2733e4
rony <rriachi@bright-lab.com>
2021-09-07 12:54:23 +0300

    some pictures with text

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Rocket() {
+function Rocket(props) {
     return (
         <div>
-            <div className = "rocket"></div>
+            <div className = "rocket text-center flex justify-end items-end text-sm text-white backdrop-filter backdrop-opacity-100">{props.title}</div>
         </div>
     )
 }
